6a250380b41bfbe94fdbd4009ae51f06ab1ba7ad
aurelien.mlk92 <aurelien.geninsimonet@gmail.com>
2024-01-18 11:12:27 +0100

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <NavBar />
     </>
   )
 }
 
-export default App
+export default App;
