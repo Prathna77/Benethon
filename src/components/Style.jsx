d274c4b7018e00eda975f9f7f9f9f314f92b04f6
27rachiid82 <mohammedikacirachid@yahoo.fr>
2024-01-19 15:10:46 +0100

    add hairs button

--- src/components/Style.jsx
+++ src/components/Style.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import "./style.css";
 
 import teintImage from "../assets/image_fond_de_teint.jpg";
@@ -6,11 +6,27 @@ import mascaraImage from "../assets/mascara.jpg";
 import rougeImage from "../assets/levres.jpg";
 import photo3 from "../assets/3.jpg";
 
+import blonde from "../assets/blonde.jpg";
+import chatain from "../assets/chatain.jpg";
+import brune from "../assets/brune.jpg";
+import rousse from "../assets/rousse.jpg";
+
 function Style() {
   const modalRef = useRef(null);
   const [menuOpen, setMenuOpen] = useState(false);
+
   const [selectedMenu, setSelectedMenu] = useState(null);
   const [selectedOption, setSelectedOption] = useState(null);
+
+  useEffect(() => {
+    const savedMenuState = localStorage.getItem("menuState");
+    if (savedMenuState) {
+      const parsedState = JSON.parse(savedMenuState);
+      setMenuOpen(parsedState.menuOpen);
+      setSelectedMenu(parsedState.selectedMenu);
+      setSelectedOption(parsedState.selectedOption);
+    }
+  }, []);
 
   const openMenu = () => {
     setMenuOpen(true);
@@ -45,46 +61,43 @@ function Style() {
   const hairColors = [
     {
       name: "Blonde",
-      color: "#ffd700",
       link: "blonde",
       href: "https://www.loreal-paris.fr/coloration/par-couleur/blonde/",
+      img: blonde,
     },
     {
       name: "Châtain",
-      color: "#33ff57",
       link: "chatain",
       href: "https://www.loreal-paris.fr/coloration/par-couleur/chatain/",
+      img: chatain,
     },
     {
       name: "Brune / Noire",
-      color: "#5733ff",
       link: "brune-noire",
       href: "https://www.loreal-paris.fr/coloration/par-couleur/brune-noire/",
+      img: brune,
     },
     {
       name: "Rousse / Auburn",
-      color: "#ff5733",
       link: "rousse-auburn",
       href: "https://www.loreal-paris.fr/coloration/par-couleur/rousse-auburn/",
+      img: rousse,
     },
-    {
-      name: "Eclaircissant",
-      color: "#33ff57",
-      link: "eclaircissant",
-      href: "https://www.loreal-paris.fr/coloration/par-couleur/eclaircissant/",
-    },
-    {
-      name: "Tie & dye et balayage",
-      color: "#5733ff",
-      link: "tie-dye-et-balayage",
-      href: "https://www.loreal-paris.fr/coloration/par-couleur/tie-dye-et-balayage/",
-    },
-    {
-      name: "Retouche racines",
-      color: "#ff5733",
-      link: "retouche-racines",
-      href: "https://www.loreal-paris.fr/coloration/par-couleur/retouche-racines/",
-    },
+    // {
+    //   name: "Eclaircissant",
+    //   link: "eclaircissant",
+    //   href: "https://www.loreal-paris.fr/coloration/par-couleur/eclaircissant/",
+    // },
+    // {
+    //   name: "Tie & dye et balayage",
+    //   link: "tie-dye-et-balayage",
+    //   href: "https://www.loreal-paris.fr/coloration/par-couleur/tie-dye-et-balayage/",
+    // },
+    // {
+    //   name: "Retouche racines",
+    //   link: "retouche-racines",
+    //   href: "https://www.loreal-paris.fr/coloration/par-couleur/retouche-racines/",
+    // },
   ];
 
   const makeupCategories = [
@@ -128,88 +141,110 @@ function Style() {
         MENU
       </button>
 
-      <div
-        id="myModal"
-        className={`modal ${menuOpen ? "open" : ""}`}
-        ref={modalRef}
-        onClick={handleWindowClick}
-      >
-        <div className="modal-content">
-          <div className="modal-header">
-            <h2>{selectedMenu || selectedOption}</h2>
-            <span onClick={closeModal}>&times;</span>
-          </div>
-          <div className="modal-body">
-            {selectedMenu === null && (
+      {menuOpen && (
+        <div
+          id="myModal"
+          className={`modal ${menuOpen ? "open" : ""}`}
+          ref={modalRef}
+          onClick={handleWindowClick}
+        >
+          <div className="modal-content">
+            <div className="modal-header">
+              <h2>{selectedMenu || selectedOption}</h2>
+              <span onClick={closeModal}>&times;</span>
+            </div>
+            <div className="modal-body">
+              {selectedMenu === null && (
+                <div className="menu-options">
+                  <button
+                    className="rachidbutton"
+                    onClick={() => openSubMenu("Coloration")}
+                  >
+                    Coloration
+                  </button>
+                  <button
+                    className="rachidbutton"
+                    onClick={() => openSubMenu("Maquillage")}
+                  >
+                    Maquillage
+                  </button>
+                  <button
+                    className="rachidbutton"
+                    onClick={() => openSubMenu("Corps")}
+                  >
+                    Soin du visage
+                  </button>
+                </div>
+              )}
+              {selectedMenu === "Coloration" && (
+                <div className="hair-color-links">
+                  {hairColors.map((hairColor, index) => (
+                    <div key={index} className="hair-color-link">
+                      <div
+                        className="hair-color-option"
+                        style={{ backgroundColor: hairColor.color }}
+                      ></div>
+                      <a
+                        href={`https://www.loreal-paris.fr/coloration/par-couleur/${hairColor.link}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {hairColor.name}
+                      </a>
 
-              <div className="menu-options">
-                <button className="rachidbutton" onClick={() => openSubMenu("Coloration")}>
-                  Coloration
-                </button>
-                <button className="rachidbutton" onClick={() => openSubMenu("Maquillage")}>
-                  Maquillage
-                </button>
-                <button className="rachidbutton" onClick={() => openSubMenu("Corps")}>
-                  Soin du visage
-                </button>
-              </div>
-            )}
-            {selectedMenu === "Coloration" && (
-              <div className="hair-color-links">
-                {hairColors.map((hairColor, index) => (
-                  <div key={index} className="hair-color-link">
+                      {hairColor.img && (
+                        <img
+                          src={hairColor.img}
+                          alt={hairColor.name}
+                          // style={{ width: '50px', height: '50px', marginLeft: '10px' }}
+                          className="rounded-image"
+                        />
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )}
+
+              {selectedMenu === "Maquillage" && (
+                <div className="makeup-categories">
+                  {makeupCategories.map((category, index) => (
+                    // <div key={index} className="makeup-category">
                     <div
-                      className="hair-color-option"
-                      style={{ backgroundColor: hairColor.color }}
-                    ></div>
-                    <a
-                      href={`https://www.loreal-paris.fr/coloration/par-couleur/${hairColor.link}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
+                      key={index}
+                      className="makeup-category"
+                      style={{ backgroundImage: `url(${category.image})` }}
                     >
-                      {hairColor.name}
-                    </a>
-                  </div>
-                ))}
-              </div>
-            )}
+                      <a
+                        href={`https://www.loreal-paris.fr/maquillage/${category.link}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {category.category}
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              )}
 
-            {selectedMenu === "Maquillage" && (
-              <div className="makeup-categories">
-                {makeupCategories.map((category, index) => (
-                  // <div key={index} className="makeup-category">
-  <div key={index} className="makeup-category" style={{ backgroundImage: `url(${category.image})` }}>
-
-                    <a
-                      href={`https://www.loreal-paris.fr/maquillage/${category.link}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {category.category}
-                    </a>
-                  </div>
-                ))}
-              </div>
-            )}
-
-            {selectedMenu === "Corps" && (
-              <div className="bodyup-categories">
-                {bodyCategories.map((gamme, index) => (
-                  <div key={index} className="bodyup-category">
-                    <a
-                      href={gamme.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {gamme.gamme}
-                    </a>
-                  </div>
-                ))}
-              </div>
-            )}
+              {selectedMenu === "Corps" && (
+                <div className="bodyup-categories">
+                  {bodyCategories.map((gamme, index) => (
+                    <div key={index} className="bodyup-category">
+                      <a
+                        href={gamme.href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {gamme.gamme}
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
